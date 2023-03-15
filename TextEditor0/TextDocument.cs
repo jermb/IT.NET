c508ed195d2686d923a27e756995ceee8f30e017
jermb <jermb@umsystem.edu>
2023-03-14 21:45:13 -0500

    Made new project and copied code

---
+++ TextEditor0/TextDocument.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TextEditor
+{
+    public class TextDocument
+    {
+        private TextBox textbox;
+
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public bool HasChanges { get; }
+
+        public TextDocument(TextBox textbox)
+        {
+            this.textbox = textbox;
+        }
+
+        public Save()
+        {
+
+        }
+
+        public Open()
+        {
+
+        }
+
+    }
+
+}
+
