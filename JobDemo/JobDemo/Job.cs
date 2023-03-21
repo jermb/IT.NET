6db837f18415464cf8cd15823c91f438f4eb4b8b
jermb <77866654+jermb@users.noreply.github.com>
2023-03-21 13:25:12 -0500

    Update Job.cs

--- JobDemo/JobDemo/Job.cs
+++ JobDemo/JobDemo/Job.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +11,25 @@ namespace JobDemo
         protected double hours;
         protected double price;
         public const double RATE = 45.00;
+
         public Job(int num, string cust, string desc, double hrs)
         {
             //TODO:
-            
+            JobNumber = num;
+            Customer = cust;
+            Description = desc;
+            Hours = hrs;
         }
 
         //TODO: auto implement property JobNumber, Customer, Description 
-        
+        public int JobNumber { get; set; }
+        public string Customer { get; set; }
+        public string Description { get; set; }
 
         //TODO: implment Hours, Price property. Consider to make Hours virtual for derived class to override
+        public double Hours { get => hours; set { hours = value; price = hours * RATE; } }
+        public double Price { get => price; }
 
- 
 
         public override string ToString()
         {
@@ -35,14 +42,20 @@ namespace JobDemo
         public override bool Equals(Object e)
         {
             //TODO:
-
-
+            try
+            {
+                return this.JobNumber == ((Job)e).JobNumber;
+            }
+            catch (InvalidCastException ex)
+            {
+                return false;
+            }
         }
 
         public override int GetHashCode()
         {
             //TODO:
-           
+            return JobNumber;
         }
 
     }
