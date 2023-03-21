80755343041d46a64beac59d49d5a7f115a98212
jermb <77866654+jermb@users.noreply.github.com>
2023-03-21 13:25:42 -0500

    Update RushJob.cs

--- JobDemo/JobDemo/RushJob.cs
+++ JobDemo/JobDemo/RushJob.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,17 +11,20 @@ namespace JobDemo
         public const double PREMIUM = 150.00;
 
         //TODO: Constructor with no parameters: public RushJob() 
-        
+        public RushJob() : base(0, null, null, 0)
+        {
+            
+        }
 
 
 
         //TODO: update Hour property with new price calculation: price = hours * RATE + PREMIUM;
-
+        public new double Hours { get => hours; set { hours = value; price = hours * RATE + PREMIUM; } }
 
         public override string ToString()
         {
             //TODO:
-
+            return $"{GetType()} {JobNumber} {Customer} {Description} {Hours} hours @{RATE.ToString("C")} per hour. Rush job adds {PREMIUM.ToString("C")}. Total Price is {Price.ToString("C")}";
         }
     }
 }
