268699192fe3cac26595b91b055cdc7fc00c53e2
jermb <jermb@umsystem.edu>
2023-04-24 10:00:30 -0500

    Added custom class

---
+++ CreatureKingdom/CreatureKingdom/EdsonJayCreature.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace CreatureKingdom
+{
+    internal class EdsonJayCreature : Creature
+    {
+        public EdsonJayCreature(Canvas kingdom, Dispatcher dispatcher, int waitTime = 100) : base(kingdom, dispatcher, waitTime)
+        {
+        }
+
+        public override void Shutdown()
+        {
+            base.Shutdown();
+        }
+    }
+}
--- CreatureKingdom/CreatureKingdom/obj/Debug/net6.0-windows/App.g.i.cs
+++ CreatureKingdom/CreatureKingdom/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace CreatureKingdom {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace CreatureKingdom {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.15.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.1.0")]
         public static void Main() {
             CreatureKingdom.App app = new CreatureKingdom.App();
             app.InitializeComponent();
